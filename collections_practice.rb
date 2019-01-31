9db629bb50341606f34df4b96db6c8611848315d
Nataliya R <n.rudenko@gmail.com>
2019-01-31 23:48:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
-# your code goes here
+def begins_with_r(array)
+  array.each do |words|
+  if words.start_with?("r")
+  return true
+end
+end
+end
+
+
+def contain_a(array)
+  array.select do |words|
+  words.include?("a")
+end
+end
+
+def first_wa(array)
+  array.find do |word|
+ word[0..1] == "wa"
+  end
+end
+
+def find_cool(cool)
+  cool.find do |cool|
+  end
+end
+  
+  
